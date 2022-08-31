bd00c46beaebee7bca85e94dee787e9b4b996fc7
Ethan Buchanan <ethcbuch@ut.utm.edu>
2022-08-30 19:17:06 -0500

    Added first project file

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Main.cpp
+ * @author Ethan Buchanan
+ * @date 2022-08-30
+ * @brief Hello World!
+ * 
+ * prints out hello world to the console
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
