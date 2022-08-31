dcceb97a128e89aa3e0c931c2d9890f6ab853549
Ethan Buchanan <ethcbuch@ut.utm.edu>
2022-08-30 19:39:52 -0500

    Added sum function

--- Main.cpp
+++ Main.cpp
@@ -12,8 +12,37 @@
 
 using namespace std;
 
+
+
+
+/**
+ * adds up numbers from 1 to num then returns the value
+ *
+ * @param int num number of numbers in the progression
+ * @pre user has given number of values in the progression
+ * @return int Total sum of the values 
+ * 
+ */
+int sum(int num);
+
 int main() {
   cout << "Hello World!" << endl;
   
+  int n = 0;
+  
+  cout << "Please enter a number: ";
+  cin >> n;
+  cout << "Total:" << sum(n) << endl;
+  
   return 0;
+}
+
+
+int sum(int num) {
+  int total = 0;
+  
+  for (int i = 0; i<=num; i++) {
+    total = total + i;
+  }
+  return total;
 }
